ff1e9d04bc38a45ceaa0ba762467b05b35e9285f
onlyati <onlyati+github@proton.me>
2025-01-05 23:29:03 +0000

    add favorite routes

--- internal/db/services/category.go
+++ internal/db/services/category.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"errors"
+
 	"github.com/onlyati/rss-collector/internal/db"
 	"gorm.io/gorm"
 )
@@ -17,7 +19,7 @@ func CreateFavoriteService(gormDB *gorm.DB, category, username string) (*db.Favo
 	}
 
 	var deletedCategory db.FavoriteCategory
-	err = gormDB.Unscoped().Where("user_id = ? and id = ?", user.ID, category).Find(&deletedCategory).Error
+	err = gormDB.Unscoped().Where("user_id = ? and name = ?", user.ID, category).Find(&deletedCategory).Error
 	if err != nil {
 		return nil, err
 	}
@@ -27,7 +29,7 @@ func CreateFavoriteService(gormDB *gorm.DB, category, username string) (*db.Favo
 			Name:   category,
 			UserID: user.ID,
 		}
-		err = gormDB.FirstOrCreate(&newCategory).Error
+		err = gormDB.Create(&newCategory).Error
 		if err != nil {
 			return nil, err
 		}
@@ -46,7 +48,7 @@ func CreateFavoriteService(gormDB *gorm.DB, category, username string) (*db.Favo
 // ===> Read
 //
 
-func ReadCategoriesService(gormDB *gorm.DB, index uint, username string) ([]db.FavoriteCategory, error) {
+func ListCategoriesService(gormDB *gorm.DB, index string, username string) ([]db.FavoriteCategory, error) {
 	var categories []db.FavoriteCategory
 	err := gormDB.Where("id > ?", index).Limit(10).Find(&categories).Error
 	if err != nil {
@@ -56,11 +58,7 @@ func ReadCategoriesService(gormDB *gorm.DB, index uint, username string) ([]db.F
 	return categories, nil
 }
 
-//
-// ===> Delete
-//
-
-func DeleteCategoryService(gormDB *gorm.DB, category, username string) (*db.FavoriteCategory, error) {
+func ReadCategoryService(gormDB *gorm.DB, category, username string) ([]db.FavoriteCategory, error) {
 	var user db.User
 	err := gormDB.Where(&db.User{UserName: username}).First(&user).Error
 	if err != nil {
@@ -68,10 +66,46 @@ func DeleteCategoryService(gormDB *gorm.DB, category, username string) (*db.Favo
 	}
 
 	var cat db.FavoriteCategory
-	err = gormDB.Where(&db.FavoriteCategory{UserID: user.ID, Name: category}).Delete(&cat).Error
+	err = gormDB.Where("user_id = ? and name = ?", user.ID, category).Find(&cat).Error
 	if err != nil {
 		return nil, err
 	}
+	if cat.Name == "" {
+		return []db.FavoriteCategory{}, nil
+	}
+	return []db.FavoriteCategory{cat}, nil
+}
 
-	return &cat, nil
+//
+// ===> Delete
+//
+
+func DeleteCategoriesHard(gormDB *gorm.DB) error {
+	var categories []db.FavoriteCategory
+	err := gormDB.Unscoped().Where("deleted_at is not null").Find(&categories).Error
+	if err != nil {
+		return err
+	}
+
+	if len(categories) == 0 {
+		return nil
+	}
+
+	err = gormDB.Unscoped().Delete(&categories).Error
+	return err
+}
+
+func DeleteCategoryService(gormDB *gorm.DB, category, username string) error {
+	var user db.User
+	err := gormDB.Where("user_name = ?", username).Find(&user).Error
+	if err != nil {
+		return err
+	}
+
+	if user.UserName == "" {
+		return errors.New("user does not exists")
+	}
+
+	err = gormDB.Where("user_id = ? and name = ?", user.ID, category).Delete(&db.FavoriteCategory{}).Error
+	return err
 }
